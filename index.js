a01643a0f48ecedda7cce49bd305d75cc66ec5e3
JCNoxoia <jcbarrett2000@protonmail.com>
2023-11-29 00:47:14 -0600

    updated api endpoint logic

--- index.js
+++ index.js
@@ -1,100 +1,164 @@
+const mongoose = require('mongoose');
+const models = require('./models.js');
+const movies = models.movie;
+const users = models.user;
+
 const express = require('express'),
-    morgan = require('morgan')
+    morgan = require('morgan');
 
 const app = express();
 
-let topMovies = [
-    {
-        title: 'Stardust',
-        releaseYear: '2007',
-        director: 'Matthew Vaughn',
-        genres: ['romance', 'fantasy', 'adventure']
-    },
-    {
-        title: 'Howl\'s Moving Castle',
-        releaseYear: '2004',
-        director: 'Hayao Miyazaki',
-        genres: ['anime', 'fantasy', 'adventure']
-    },
-    {
-        title: 'Hot Fuzz',
-        releaseYear: '2007',
-        director: 'Edgar Wright',
-        genres: ['action', 'comedy']
-    },
-    {
-        title: 'The Mask',
-        releaseYear: '1994',
-        director: 'Chuck Russell',
-        genres: ['comedy']
-    },
-    {
-        title: 'Django Unchained',
-        releaseYear: '2012',
-        director: 'Quentin Tarantino',
-        genres: ['action', 'western']
-    },
-    {
-        title: 'Spirited Away',
-        releaseYear: '2001',
-        director: 'Hayao Miyazaki',
-        genres: ['anime', 'fantasy', 'adventure']
-    },
-    {
-        title: 'Idiocracy',
-        releaseYear: '2006',
-        director: 'Mike Judge',
-        genres: ['Comedy']
-    },
-    {
-        title: 'Monty Python and The Holy Grail',
-        releaseYear: '1975',
-        director: 'Terry Gilliam & Terry Jones',
-        genres: ['adventure', 'fantasy', 'comedy']
-    },
-    {
-        title: 'The Thing',
-        releaseYear: '1982',
-        director: 'John Carpenter',
-        genres: ['Horror', 'Thriller']
-    },
-    {
-        title: 'Get Smart',
-        releaseYear: '2008',
-        director: 'Peter Segal',
-        genres: ['action', 'comedy']
-    }
-];
+
+mongoose.connect('mongodb://localhost:27017/jmdDB');
+
+//body-parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 //Server logging
 app.use(morgan('common'));
 
 //CREATE requests
-app.post('/users', (req, res) => res.send('Text confirmation that the new user has been created.'));
+app.post('/users', async (req, res) => {
+    await users.findOne({username: req.body.username})
+        .then((user) => {
+            if (user) {
+                return res.status(400).send(req.body.username + ' already exists.');
+            } else {
+              users
+                .create({
+                    username: req.body.username,
+                    password: req.body.password,
+                    email: req.body.email,
+                    dob: req.body.dob
+                })
+                .then((user) => {res.status(201).json(user)})
+                .catch((err) => {
+                    console.error(err);
+                    res.status(500).send('Error: ' + err);
+                })
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
 
-app.post('/users/:id/:title', (req, res) => res.send('Text confirmation that the indicated movie has been added to the favorites list.'));
+app.post('/users/:username/:title', async (req, res) => {
+    await movies.findOne({title: req.params.title})
+        .then((movie) => {
+            users.updateOne(
+            {username: req.params.username},
+            {$push: {favMovies: movie._id}}
+        )})
+        .then((user) => {
+            res.status(201).send('Successfully added ' + req.params.title + ' to ' + user.username + '\'s favorites list.')
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
 
 
 //RETRIEVE requests:
 app.get('/', (req, res) => res.send('Welcome to the JMD Movie Database.'));
 
-app.get('/movies', (req, res) => res.json(topMovies));
+app.get('/movies', async (req, res) => {
+    await movies.find()
+        .then((movies) => {
+            res.status(200).json(movies);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
 
-app.get('/movies/:title', (req, res) => res.send('JSON Object with data on a single movie.'));
+app.get('/movies/:title', async (req, res) => {
+    await movies.findOne({title: req.params.title})
+        .then((movies) => {
+            res.status(200).json(movies);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
 
-app.get('/genres/:genreName', (req, res) => res.send('JSON Object with data on the genre.'));
+app.get('/genres/:genreName', async (req, res) => {
+    await movies.findOne({'genre.name': req.params.genreName})
+        .then((movies) => {
+            res.status(200).json(movies.genre);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
 
-app.get('/directors/:directorName', (req, res) => res.send('JSON Object with data on the director.'));
+app.get('/directors/:directorName', async (req, res) => {
+    await movies.findOne({'director.name': req.params.directorName})
+        .then((movies) => {
+            res.status(200).json(movies.director);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
 
 
 //UPDATE requests:
-app.put('/users/:id', (req, res) => res.send('Text confirmation that the user\'s ID has been updated.'));
+app.put('/users/:username', async (req, res) => {
+    await users.findOneAndUpdate(
+        {username: req.params.username},
+        {$set: {
+            username: req.body.username,
+            password: req.body.password,
+            email: req.body.email,
+            dob: req.body.dob
+        }})
+        .then((user) => {
+            res.status(200).send(user.username + '\'s account successfully updated.')
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
 
 
 //DELETE requests:
-app.delete('/users/:id/:title', (req, res) => res.send('Text confirmation that the indicated movie has been removed.'));
+app.delete('/users/:username/:title', async (req, res) => {
+    await users.findOneAndUpdate(
+        {username: req.params.username},
+        {$pull: {favMovies: req.params.title}}
+    )
+    .then((user) => {
+        res.status(200).send(req.params.title + ' has been successfully removed from ' + user.username + '\'s favorites list.')
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
+});
 
-app.delete('/users/:id', (req, res) => res.send('Text confirmation that the indicated user account has been successfully deleted.'));
+app.delete('/users/:username', async (req, res) => {
+    await users.deleteOne({username: req.params.username})
+        .then((user) => {
+            if (!user) {
+                res.status(400).send('User was not found.');
+            } else {
+                res.status(200).send('User successfully deleted.');
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
 
 
 //Public static file serving
@@ -106,4 +170,5 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something went wrong...');
 });
 
+//Run app
 app.listen(8080, () => console.log('App is listening on Port 8080.'));
---
+++ models.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+let movieSchema = mongoose.Schema({
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    genre: {
+        name: String,
+        description: String
+    },
+    director: {
+        name: String,
+        dob: Date,
+        dod: Date
+    },
+    featured: Boolean
+});
+
+let userSchema = mongoose.Schema({
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    email: {type: String, required: true},
+    dob: Date,
+    favMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'movie'}]
+});
+
+let movie = mongoose.model('movie', movieSchema);
+let user = mongoose.model('user', userSchema);
+
+module.exports.movie = movie;
+module.exports.user = user;
